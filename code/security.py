4fd8ee2709c76c9db2ed2a362651101ac78c5add
Joseorina <orinangare@gmail.com>
2019-10-22 15:14:27 +0300

    security

---
+++ code/security.py
@@ -0,0 +1,26 @@
+users = [
+    {
+        'id': 1,
+        'username': 'bob',
+        'password': 'asdf'
+    }
+]
+
+#has username as key
+username_mapping = { 'bob:':{
+        'id': 1,
+        'username': 'bob',
+        'password': 'asdf'
+    }
+}
+
+# userid_mapping[1]
+# username_mapping['bob']
+
+#has userid as mapping
+userid_mapping = { 1: {
+        'id': 1,
+        'username': 'bob',
+        'password': 'asdf'
+    }
+}

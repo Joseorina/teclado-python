37d0c6409d5be4e1f748b73fbe572494bf193289
Joseorina <orinangare@gmail.com>
2019-10-22 15:29:57 +0300

    Authentication and login

--- code/security.py
+++ code/security.py
@@ -1,3 +1,4 @@
+from werkzeug.security import safe_str_cmp
 from user import User
 
 users = [
@@ -10,7 +11,7 @@ userid_mapping = {u.id: u for u in users}
 
 def authenticate(username, password):
     user = userid_mapping.get(username, None)
-    if user and user.password == password:
+    if user and safe_str_cmp(user.password, password):
         return user
     
 def identity(payload):

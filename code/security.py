a6b3b23c214d8731524b708c1c613dbac7824962
Joseorina <orinangare@gmail.com>
2019-10-22 15:20:08 +0300

    Authentication adnlogging in

--- code/security.py
+++ code/security.py
@@ -24,3 +24,12 @@ userid_mapping = { 1: {
         'password': 'asdf'
     }
 }
+
+def authenticate(username, password):
+    user = userid_mapping.get(username, None)
+    if user and user.password == password:
+        return user
+    
+def identity(payload):
+    user_id = payload['identity']
+    return userid_mapping(user_id, None)

244104e42a13d3b09dd24cc940ea42aee3b19cb4
Joseorina <orinangare@gmail.com>
2019-10-22 15:27:05 +0300

    AUthentication and login

--- code/security.py
+++ code/security.py
@@ -1,29 +1,12 @@
+from user import User
+
 users = [
-    {
-        'id': 1,
-        'username': 'bob',
-        'password': 'asdf'
-    }
+    User(1, 'bob','asdf')
 ]
 
-#has username as key
-username_mapping = { 'bob:':{
-        'id': 1,
-        'username': 'bob',
-        'password': 'asdf'
-    }
-}
+username_mapping = {u.username: u for u in users}
 
-# userid_mapping[1]
-# username_mapping['bob']
-
-#has userid as mapping
-userid_mapping = { 1: {
-        'id': 1,
-        'username': 'bob',
-        'password': 'asdf'
-    }
-}
+userid_mapping = {u.id: u for u in users}
 
 def authenticate(username, password):
     user = userid_mapping.get(username, None)

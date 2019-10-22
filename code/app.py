24511a94b3fa1d4e466d7ecb9f90bcac24849103
Joseorina <orinangare@gmail.com>
2019-10-22 17:47:07 +0300

    delete

--- code/app.py
+++ code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource,Api
+from flask_restful import Resource,Api, reqparse
 from flask_jwt import  JWT, jwt_required
 
 from security import authenticate, identity

4468a94ad49608c27b8586565b72762f807b94e1
Joseorina <orinangare@gmail.com>
2019-10-22 14:23:16 +0300

    restful api

--- code/app.py
+++ code/app.py
@@ -4,9 +4,18 @@ from flask_restful import Resource,Api
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
+items = []
+class Item(Resource):
     def get(self, name):
-        return {'student':name}
+        for item in items:
+            if item['name'] == name:
+                return item
+
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item
     
-api.add_resource(Student,'/student/<string:name>')
-app.run(port = 5000)
+api.add_resource(Item, '/item/<sting:name>')
+
+app.run(port=5000)

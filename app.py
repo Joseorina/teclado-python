a02fcb8764df1e1406b601367f752387a65f7884
Joseorina <orinangare@gmail.com>
2019-10-22 12:36:17 +0300

    python flask

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-"""
-flask app
-"""
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-	return 'hello, world'
+stores = [
+	{
+		'name':'My store',
+		'items':[{
+				'name':'My item',
+				'price':15.99
+				}
+   			] 
+	}
+]
+#POST /store data: {name}
+@app.route('/store',methods=['POST'])
+def create_store():
+	pass
 
+#GET /store/<stringL:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+	pass
+
+#GET /store
+@app.route('/store')
+def get_stores():
+	pass
+
+@app.route('/store/<string:name>/item',methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    pass
+ 
 app.run(port = 5000)	
